09ef2ab5461e9fa6c1daa3fa07e9757994139346
Anton Agaronov <144243055+AntonAgaronov@users.noreply.github.com>
2025-07-24 11:47:08 +0400

    feat: add types to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { useFormWithValidation } from '@/composables/formValidation/useFormWithValidation.ts';
 
 export { default as BaseButton } from '@/components/button/BaseButton.vue';
+export { ButtonType } from '@/components/button/ButtonTypes';
 
 export { useFormWithValidation };

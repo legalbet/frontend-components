4b64242abb48dba8fae1ca7112eefeb0bbef89c7
Anton Agaronov <144243055+AntonAgaronov@users.noreply.github.com>
2025-07-24 13:47:54 +0400

    feat: export ButtonType not how only type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { useFormWithValidation } from './composables/formValidation/useFormWithValidation.ts';
 
 export { default as BaseButton } from './components/button/BaseButton.vue';
-export type { ButtonType } from './components/button/ButtonTypes';
+export { ButtonType } from './components/button/ButtonTypes';

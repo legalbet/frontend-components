4e9c7731dc54d6b780523800cfcd6a54a55f6271
Fanil <fanil.zaynetdinov@legalbet.com>
2025-07-29 12:46:55 +0500

    base checkbox (#2)
    component create

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { useFormWithValidation } from './composables/formValidation/useFormWithV
 
 export { default as BaseButton } from './components/button/BaseButton.vue';
 export { ButtonType } from './components/button/ButtonTypes';
+
+export { default as BaseCheckbox } from './components/checkbox/BaseCheckbox.vue';
